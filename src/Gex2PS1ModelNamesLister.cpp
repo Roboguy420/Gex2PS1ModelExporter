d950ef93c58146da0089592c2260b0114af58980
Roboguy420 <wahaller@hotmail.co.uk>
2023-10-08 16:44:47 +0100

    Included the full GPLv3 copyright notice found in Gex2PS1ModelExporter.cpp, in all source files

--- src/Gex2PS1ModelNamesLister.cpp
+++ src/Gex2PS1ModelNamesLister.cpp
@@ -1,6 +1,18 @@
-/*  Copyright (C) 2023  Roboguy420
+/*  Gex2PS1ModelExporter: Command line program for exporting Gex 2 PS1 models
+	Copyright (C) 2023  Roboguy420
 
-    Full copyright notice in Gex2PS1ModelExporter.cpp  */
+	Gex2PS1ModelExporter is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Gex2PS1ModelExporter is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Gex2PS1ModelExporter.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include "Gex2PS1SharedFunctions.h"
 
