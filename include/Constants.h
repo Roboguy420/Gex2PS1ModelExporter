fff93f19db2f2d4b6921ab79b7a44104131b4abe
Roboguy420 <wahaller@hotmail.co.uk>
2023-12-19 16:48:03 +0000

    Added GPLv3 to source and header files (again)

--- include/Constants.h
+++ include/Constants.h
@@ -1,3 +1,19 @@
+/*  Gex2PS1ModelExporter: Command line program for exporting Gex 2 PS1 models
+    Copyright (C) 2023  Roboguy420
+
+    Gex2PS1ModelExporter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gex2PS1ModelExporter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gex2PS1ModelExporter.  If not, see <https://www.gnu.org/licenses/>.  */
+
 #define EXIT_SUCCESSFUL_EXPORT 0
 #define EXIT_INSUFFICIENT_ARGS 1
 #define EXIT_BAD_ARGS 2
